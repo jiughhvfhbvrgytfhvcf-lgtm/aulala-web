e197890a37de7edf8823b74e921a51c34ad13120
jiughhvfhbvrgytfhvcf-lgtm <jiughhvfhbvrgytfhvcf@gmail.com>
2026-03-24 23:49:50 +0800

    Add files via upload

---
+++ orders-data.js
@@ -0,0 +1,36 @@
+window.AULALA_ORDER_FEED = {
+  "generatedAt": "2026-03-24 23:26:38",
+  "source": "fbadidi-erp-bridge",
+  "rules": {
+    "pickupAddress": "TLA",
+    "priceAud": 100.0,
+    "cbmPerPallet": 1.5,
+    "expectedPickupTime": "actual_unpack_time + 1 day"
+  },
+  "orders": [
+    {
+      "id": 1001,
+      "orderCode": "MY04000060XBW3",
+      "price": 100.0,
+      "state": "NSW",
+      "pickup": "TLA",
+      "dropoff": "Kemps Creek",
+      "pallets": 1,
+      "weight": "55.15kg",
+      "time": "25-03 10:35",
+      "date": "2026-03-25",
+      "pickupAddress": "TLA",
+      "dropoffAddress": "19 Giba Drive, Kemps Creek, Kemps Creek, NSW, 2178",
+      "pieces": 3,
+      "cbm": 0.54,
+      "weightKg": 55.15,
+      "actualUnpackTime": "2026-03-24 10:35",
+      "expectedPickupTime": "2026-03-25 10:35",
+      "clearanceStatus": "clean",
+      "chargeCode": "2179",
+      "refId": "SKP3103045031",
+      "source": "fbadidi-erp-bridge",
+      "sourceRefreshedAt": "2026-03-24 21:53:31"
+    }
+  ]
+};
