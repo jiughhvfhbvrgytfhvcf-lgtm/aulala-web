ee8529f82844eef39425fc8a35e9069a6df9099d
jiughhvfhbvrgytfhvcf-lgtm <jiughhvfhbvrgytfhvcf@gmail.com>
2026-04-16 04:00:12 +0800

    Sync Aulala ERP order feed (0 orders)

--- orders-data.js
+++ orders-data.js
@@ -1,5 +1,5 @@
 window.AULALA_ORDER_FEED = {
-  "generatedAt": "2026-04-15 06:00:01",
+  "generatedAt": "2026-04-16 06:00:02",
   "source": "fbadidi-live-crowdfund-api",
   "rules": {
     "pickupAddress": "悉尼",
